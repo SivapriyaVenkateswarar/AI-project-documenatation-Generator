dba99a14fe22d9e26b92a7f42a14169fa3467254
Siya <160723353+SivapriyaVenkateswarar@users.noreply.github.com>
2025-06-06 09:14:18 +0530

    Delete github_repo/running.py

--- github_repo/running.py
+++
@@ -1,9 +0,0 @@
-from cloning_github import clone_repo
-from metadata_directory import build_tree_with_metadata
-from saving import save_structure
-
-if __name__ == "__main__":
-    url = "https://github.com/SivapriyaVenkateswarar/ThreatLens"
-    clone_repo(url)
-    struct = build_tree_with_metadata("P:\AI_Documentation\github_repo\cloned_repos")
-    save_structure(struct)
